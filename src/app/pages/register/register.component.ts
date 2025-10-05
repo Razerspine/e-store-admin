509033a10fc94eca4e1cc271240cca37797bbbe1
bagel <ponomarenko.pavel.web@gmail.com>
2025-10-05 16:05:08 +0300

    add role select for register form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Button} from 'primeng/button';
 import {IconField} from 'primeng/iconfield';
@@ -8,17 +8,20 @@ import {Password} from 'primeng/password';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AuthService} from '@core/services';
 import {matchPasswordValidator} from '@core/validators';
+import {ROLE_CONFIG} from '@core/configs';
+import {Select} from 'primeng/select';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, Button, IconField, InputIcon, InputText, Password, ReactiveFormsModule],
+  imports: [CommonModule, Button, IconField, InputIcon, InputText, Password, ReactiveFormsModule, Select],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
+  roles = signal(ROLE_CONFIG);
   form: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
